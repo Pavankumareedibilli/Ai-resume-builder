0b65b7e01737d344bd8d0df830bf550cdab88db8
pavan <pavankumarpk1923@gmail.com>
2025-09-21 15:04:08 +0530

    Tested shadcn compnent avatar- Test status: success

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
-import { Button } from './components/ui/button'
-import './App.css'
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+import "./App.css";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
       <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+        <Button>Click me</Button>
+      </div>
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
